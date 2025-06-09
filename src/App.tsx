769c6a120734f8963db84f01e6d3fe7ebf77e5cd
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-06-09 14:07:23 -0700

    logic finished and starting desine

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { Route, Routes} from "react-router"
+import { Navigate, Route, Routes} from "react-router"
 import Sign_in from "./pages/Sign_in"
 import Sign_up from "./pages/Sign_up"
 import  Home  from "./pages/Home"
 import "./index.css";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/my components/ProtectedRoute";
 
 function App() {
   return (
     <div className="bgCol dark:bgCol transition duration-700">
     <div className="flex flex-col items-center justify-center min-h-svh">
 
-        <Routes>
-          <Route path="/" element={<Sign_in />}/>
-          <Route path="/Creat" element={<Sign_up />}/>
-          <Route path="/Home" element={<Home />}/>
-        </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Sign_in />} />
+        <Route path="/Creat" element={<Sign_up />} />
+        <Route
+          path="/Home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </AuthProvider>
 
     </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,15 @@ import { createRoot } from "react-dom/client";
 import ThemeProvider from "./contexts/ThemeContext";
 import "./index.css";
 import App from "./App.tsx";
-import { AuthProvider } from "./contexts/AuthContext.tsx";
 import { BrowserRouter as Router} from "react-router"
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-    <AuthProvider>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </AuthProvider>
     </Router>
   </StrictMode>
 );
